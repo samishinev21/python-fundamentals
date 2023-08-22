5ff4b4872d717de94b780cb4697ef697edb9a6fe
Sammy Mishinev <sam.mishinev@gmail.com>
2023-08-22 20:10:17 +0300

    Add shopping

---
+++ 01-basic-syntax-conditional-statements-and-loops/06-shopping.py
@@ -0,0 +1,12 @@
+budget = int(input())
+command = input()
+
+while command != "End":
+    price_products = int(input())
+    budget -= price_products
+    if budget < 0:
+        print("You went in overdraft!")
+        break
+    command = str(input())
+else:
+    print("You bought evrything you need.")
