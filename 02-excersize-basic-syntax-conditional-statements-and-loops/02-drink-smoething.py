d39e568c68f081cc7dc7c145b12cad306e9e3614
Sammy Mishinev <sam.mishinev@gmail.com>
2023-08-24 14:14:46 +0300

    Add drink something

---
+++ 02-excersize-basic-syntax-conditional-statements-and-loops/02-drink-smoething.py
@@ -0,0 +1,10 @@
+age = int(input())
+
+if age <= 14:
+    print("drink toddy")
+elif age > 14 and age <= 18:
+    print("drink coke")
+elif age > 18 and age <= 21:
+    print("drink bear")
+elif age > 21:
+    print("drink whisky")
