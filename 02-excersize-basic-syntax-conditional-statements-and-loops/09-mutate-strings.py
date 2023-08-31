d546c3f95bef89d6cd3eac495a89227888f6d4fb
Sammy Mishinev <sam.mishinev@gmail.com>
2023-08-31 20:37:47 +0300

    Fix a bug

--- 02-excersize-basic-syntax-conditional-statements-and-loops/09-mutate-strings.py
+++ 02-excersize-basic-syntax-conditional-statements-and-loops/09-mutate-strings.py
@@ -2,5 +2,9 @@ string_0 = input()
 string_1 = input()
 
 for i, letter in enumerate(string_1):
+    if letter == string_0[i]:
+        continue
+
     string_0 = string_0[:i] + letter + string_0[i + 1:]
+    
     print(string_0)
