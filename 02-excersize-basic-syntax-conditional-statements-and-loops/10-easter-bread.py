f644ae27ecba6df75ceadc53e849e9e227836819
Sammy Mishinev <sam.mishinev@gmail.com>
2023-09-07 19:37:15 +0300

    Add easter bread calculations

--- 02-excersize-basic-syntax-conditional-statements-and-loops/10-easter-bread.py
+++ 02-excersize-basic-syntax-conditional-statements-and-loops/10-easter-bread.py
@@ -6,5 +6,8 @@ milk_price = (flour_price_per_kg + (flour_price_per_kg * 25 / 100)) / 4
 price_bread = flour_price_per_kg + pack_eggs_price + milk_price
 count_breads = budget // price_bread
 colored_eggs = count_breads * 3
-print(count_breads)
-print(colored_eggs)
+lost_eggs = count_breads - 2
+colored_eggs -= lost_eggs
+budget_left = budget - price_bread * count_breads
+
+print(f"You made {int(count_breads)} loaves of Easter bread! Now you have {int(colored_eggs)} eggs and {budget_left:.2f}BGN left.")
