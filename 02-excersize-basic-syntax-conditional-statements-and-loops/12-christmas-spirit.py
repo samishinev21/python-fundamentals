6ede13082f9bb0b77e816ebe879bcfd656619de8
Sammy Mishinev <sam.mishinev@gmail.com>
2023-09-08 19:38:32 +0300

    Add more details to christmas spirit

--- 02-excersize-basic-syntax-conditional-statements-and-loops/12-christmas-spirit.py
+++ 02-excersize-basic-syntax-conditional-statements-and-loops/12-christmas-spirit.py
@@ -16,12 +16,26 @@ lights_price = 15
 lights_points = 17
 
 for day in range(1, days + 1):
+    if day % 10 == 0:
+        points -= 20
+        budget += (lights_price + skirt_price + ornaments_price)
+    if day % 11 == 0:
+        quantity += 2
     if day % 2 == 0:
-        budget += ornaments_price
+        budget += ornaments_price * quantity
         points += ornaments_poits
     if day % 3 == 0:
-        budget += skirt_price + garlands_price
+        budget += (skirt_price + garlands_price) * quantity
         points += skirt_points + garlands_points
     if day % 5 == 0:
-        budget += lights_price
+        budget += lights_price * quantity
         points += lights_points
+    if day % 3 == 0 and day % 5 == 0:
+        points += 30
+        
+if days == 10:
+    points -= 30
+
+print(f"Total cost: {budget}")
+print(f"Total spirit: {points}")
+    
