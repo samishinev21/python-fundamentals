afe3a138094b1cc4479c8a482b8e4c3a5d35ede0
Sammy Mishinev <sam.mishinev@gmail.com>
2023-09-15 19:33:40 +0300

    Fix patterns

--- 01-lab-basic-syntax-conditional-statements-and-loops/07-patterns.py
+++ 01-lab-basic-syntax-conditional-statements-and-loops/07-patterns.py
@@ -2,10 +2,10 @@ number = int(input())
 
 for number_of_stars in range(1, number + 1):
     for _ in range(number_of_stars):
-        print("*", end=" ")
+        print("*", end="")
     print("")
 
 for number_of_stars in range(number -1, 0, -1):
     for _ in range(number_of_stars):
-        print("*", end=" ")
+        print("*", end="")
     print("")
