c9f4ad2dc0d171512c855df8396730407a2a2ee8
Sammy Mishinev <sam.mishinev@gmail.com>
2023-09-21 20:08:00 +0300

    Add integer oparations

--- 05-lab-data-types-and-variables/04-centuries-to-minutes.py
+++ 05-lab-data-types-and-variables/04-centuries-to-minutes.py
@@ -5,4 +5,4 @@ days = int(years * 365.2422)
 hours = days * 24
 minutes = hours * 60
 
-print(f"{centuries} centuries = {years} years = {days} days = {int(hours)} hours = {int(minutes)} minutes")
+print(f"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes")
---
+++ 06-excersise-data-types-and-variables/01-integer-operations.py
@@ -0,0 +1,11 @@
+number_0 = int(input())
+number_1 = int(input())
+number_2 = int(input())
+number_3 = int(input())
+
+number = number_0 + number_1
+number = number // number_2
+number = number * number_3
+
+print(number)
+
