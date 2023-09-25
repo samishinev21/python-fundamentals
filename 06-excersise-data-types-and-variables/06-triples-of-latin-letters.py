8c69b077ac6282628c8110f35c9b93653aa6d7c4
Sammy Mishinev <sam.mishinev@gmail.com>
2023-09-25 10:14:25 +0300

    Fix triples of latin letters

--- 06-excersise-data-types-and-variables/06-triples-of-latin-letters.py
+++ 06-excersise-data-types-and-variables/06-triples-of-latin-letters.py
@@ -3,4 +3,4 @@ num = int(input())
 for n in range(num):
     for m in range(num):
         for v in range(num):
-            print(chr(97 + n), chr(97 + m), chr(97 + v))
+            print(f"{chr(97 + n)}{chr(97 + m)}{chr(97 + v)}")
