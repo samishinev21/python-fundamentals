c5bab6466382054b68460c3aa4b339738f3d8201
Sammy Mishinev <sam.mishinev@gmail.com>
2023-09-28 10:09:40 +0300

    Fix search

--- 07-lists-basics/04-search.py
+++ 07-lists-basics/04-search.py
@@ -13,3 +13,20 @@ for sentence in massive:
         massive.remove(sentence)
 
 print(massive)
+
+# n = int(input())
+# word = input()
+# strings = []
+
+# for _ in range(n):
+#     current_strings = input()
+#     strings.append(current_strings)
+
+# print(strings)
+
+# for i in range(len(strings) - 1, -1, -1):
+#     element = strings[i]
+#     if word not in element:
+# 	    strings.remove(element)
+
+# print(strings)
