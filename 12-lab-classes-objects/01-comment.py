3db3f00a37228a2993f3d3dbce87527243c10c77
Sammy Mishinev <sam.mishinev@gmail.com>
2023-10-26 20:34:46 +0300

    Add comment

---
+++ 12-lab-classes-objects/01-comment.py
@@ -0,0 +1,17 @@
+class Comment:
+    def __init__(self, username, content, likes = 0):
+        self.username = username
+        self.content = content
+        self.likes = likes
+
+comment = Comment("user1", "I like the book")
+
+print(comment.username)
+print(comment.content)
+print(comment.likes)
+
+comment.likes += 1
+
+print(comment.username)
+print(comment.content)
+print(comment.likes)
