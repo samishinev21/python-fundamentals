8beecd0c5f0900a5d893e4c87f23e4e57d154979
Sammy Mishinev <sam.mishinev@gmail.com>
2023-11-08 10:32:19 +0200

    Add statistics

---
+++ 14-lab-dictionaries/03-statistics.py
@@ -0,0 +1,24 @@
+stock = {}
+command = input()
+
+while command != "statistics":
+    tokens = command.split(": ")
+    key = tokens[0]
+    value = tokens[1]
+
+    if key in stock:
+        stock[key] = stock[key] + int(value)
+    else:
+        stock[key] = int(value)
+
+    print(stock)
+
+    command = input()
+
+print("Products in stock:")
+
+for (product, quantity) in stock.items():
+    print(f"- {product}: {quantity}")
+
+print(f"Total Products: {len(stock.keys())}")
+print(f"Total Quantity: {sum(stock.values())}")
